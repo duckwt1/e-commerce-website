af7ca8a97baa3da381b0db689d98f5b1927ab46a
Duck <tranquangviet92nbk@gmail.com>
2024-10-19 19:20:44 +0700

    beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Navbar from "./layouts/header-footer/Navbar";
+import Footer from "./layouts/header-footer/Footer";
+import Homepage from "./layouts/pages/homepage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+    <Navbar/>
+      {/* Navbar */}
+
+        {/* Homepage */}
+        <Homepage/>
+        {/* Homepage */}
+
+        {/* Footer */}
+     <Footer/>
     </div>
   );
 }
